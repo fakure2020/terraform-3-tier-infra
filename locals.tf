3fda566f5d6384d04399ec24798551c0e00c41d9
mohammad fakure <fakure1994@gmail.com>
2023-11-08 22:33:20 -0500

    we make america great

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  project_tags = {
+    coantact = "devops@jjtech.com"
+    application = "payments"
+    project = "jjtech"
+    environment = "${terraform.workspace}"
+    creationTime = timestamp()
+  }
+}
---
+++ vpc/varaible.tf
@@ -0,0 +1,43 @@
+variable "vpc_cidr" {
+  type = string
+  description = "VPC CIDR Range"
+  default = "10.0.0.0/16" 
+}
+
+
+variable "public_subnet1_cidr_block" {
+  description = "CIDR block for the public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet2_cidr_block" {
+  description = "CIDR block for the public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet1_cidr_block" {
+  description = "CIDR block for the private subnet 1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet2_cidr_block" {
+  description = "CIDR block for the private subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability zone for public_subnet1"
+  default = [ "us-east-1a", "us-east-1b" ]
+}
+
+variable "tags" {
+  type = map
+  description = "tags"
+  
+}
